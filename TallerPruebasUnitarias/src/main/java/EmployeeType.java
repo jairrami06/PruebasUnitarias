f38edd79028bee99323bf1e0622bcd330f8a514c
jairrami06 <117552816+jairrami06@users.noreply.github.com>
2023-08-21 09:46:44 -0500

    Commit inicial

---
+++ TallerPruebasUnitarias/src/main/java/EmployeeType.java
@@ -0,0 +1,1 @@
+public enum EmployeeType { Worker, Supervisor, Manager }
---
+++ TallerPruebasUnitarias/src/main/java/com/mycompany/tallerpruebasunitarias/TallerPruebasUnitarias.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tallerpruebasunitarias;
+
+/**
+ *
+ * @author jaira
+ */
+public class TallerPruebasUnitarias {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
